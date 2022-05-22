dbef2a432878c58eb766f79e1aebaef5a532dc36
Amier Putra Azhar <86624398+amierputra@users.noreply.github.com>
2022-05-22 09:09:12 +0800

    form validation

--- mytutor/lib/views/loginscreen.dart
+++ mytutor/lib/views/loginscreen.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:mytutor/views/mainscreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,75 +13,113 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late double screenHeight, screenWidth;
+  late double screenHeight, screenWidth, ctrwidth;
   bool remember = false;
   TextEditingController emailCtrller = TextEditingController();
   TextEditingController passwordCtrller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     screenHeight = MediaQuery.of(context).size.height;
     screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth >= 800) {
+      ctrwidth = screenWidth / 1.5;
+    }
+    if (screenWidth < 800) {
+      ctrwidth = screenWidth;
+    }
     return Scaffold(
         body: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+      child: Center(
+        child: SizedBox(
+          width: ctrwidth,
+          child: Form(
+            key: _formKey,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                    height: screenHeight / 2.5,
-                    width: screenWidth,
-                    child: Image.asset('assets/images/mytutor.png')),
-                const Text(
-                  "Login",
-                  style: TextStyle(fontSize: 24),
-                ),
-                const SizedBox(height: 20),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                  child: TextField(
-                    controller: emailCtrller,
-                    decoration: InputDecoration(
-                        hintText: "Email",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0))),
-                    keyboardType: TextInputType.emailAddress,
+                  padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                          height: screenHeight / 2.5,
+                          width: screenWidth,
+                          child: Image.asset('assets/images/mytutor.png')),
+                      const Text(
+                        "Login",
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                        child: TextFormField(
+                          controller: emailCtrller,
+                          decoration: InputDecoration(
+                              hintText: "Email",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0))),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter valid email';
+                            }
+                            bool emailValid = RegExp(
+                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value);
+
+                            if (!emailValid) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                        child: TextFormField(
+                          controller: passwordCtrller,
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0))),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            if (value.length < 6) {
+                              return "Password must be at least 6 characters";
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(value: remember, onChanged: _onRememberMe),
+                          const Text("Remember Me")
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: screenWidth,
+                        height: 50,
+                        child: ElevatedButton(
+                          child: const Text("Login"),
+                          onPressed: _loginUser,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-                  child: TextField(
-                    controller: passwordCtrller,
-                    decoration: InputDecoration(
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0))),
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Checkbox(value: remember, onChanged: _onRememberMe),
-                    const Text("Remember Me")
-                  ],
-                ),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: screenWidth,
-                  height: 50,
-                  child: ElevatedButton(
-                    child: const Text("Login"),
-                    onPressed: _loginUser,
-                  ),
-                )
               ],
             ),
           ),
-        ],
+        ),
       ),
     ));
   }
